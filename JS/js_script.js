9c8d5a83639ab74f2759a11daa552c67a3948d74
ryorri <rafal.zadlo@microsoft.wsei.edu.pl>
2023-06-05 17:28:24 +0200

    Loading bars

--- JS/js_script.js
+++ JS/js_script.js
@@ -1,6 +1,18 @@
+
 function disable() {
     let input = document.getElementById('toggle');
 	input.checked = false;
 	
 }
+		
+function load()	{
+var element1 = document.getElementById("ui");
+  element1.classList.add("loading");
+  
+  var element2 = document.getElementById("web");
+  element2.classList.add("loading");
+  
+  var element3 = document.getElementById("marketing");
+  element3.classList.add("loading");
 
+}
